5654d6b615483a5c145363accb8bd820b47accb5
Christian Schweinhardt <are.u.kidding@me.com>
2021-08-18 13:24:31 +0200

    fix(entrypoint): install requirements

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,18 +10,21 @@ REQS=${GITHUB_WORKSPACE}/${DEP_PATH}
 echo -e "\n----------------- Variables -------------------"
 echo -e "${COLOR_YELLOW}INFO${COLOR_OFF} Requirements path: ${REQS}"
 
+echo -e "\n------------ Install requirements -------------"
+pip install -r $REQS
+
 echo -e "\n-------------- Do license check ---------------"
 echo -e "${COLOR_YELLOW}INFO${COLOR_OFF} Call command: liccheck -r ${REQS} &> output.txt"
 liccheck -r $REQS &> output.txt
 EXIT_CODE=$?
 
-echo -e "\n--------- Python License Check results --------"
+echo -e "\n-------- Python License Check results ---------"
 echo -e "${COLOR_YELLOW}INFO${COLOR_OFF} Call command: cat output.txt"
 cat output.txt
 
 if [ "$EXIT_CODE" -eq 0 ]
 then
-    echo -e "\n${COLOR_GREEN}SUCCESS${COLOR_OFF} exit 0"
+    echo -e "\n${COLOR_GREEN}SUCCESS${COLOR_OFF}"
     exit 0
 else
     echo -e "\n${COLOR_RED}ERROR${COLOR_OFF} exit ${EXIT_CODE}"
